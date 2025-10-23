0d550c78527b8e091808f7cd22aab1adc46e03aa
kushalbakshi <kushalbakshi@gmail.com>
2025-10-23 16:15:18 -0400

    fix: exclude notebooks and images from packages in repo

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license="MIT",
     url=f'https://github.com/datajoint/{pkg_name.replace("_", "-")}',
     keywords="neuroscience miniscope science datajoint",
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    packages=find_packages(exclude=["contrib", "docs", "tests*", "notebooks", "images"]),
     scripts=[],
     install_requires=[
         "datajoint>=0.14.4",
