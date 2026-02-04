1b6263910a371956eabe04a6c0bed3a4354dc191
Kushal Bakshi <kushalbakshi@gmail.com>
2026-02-04 12:37:04 -0500

    version pin xarray

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "tests": ["pytest", "pytest-cov", "shutils"],
         "minian": [
             "dask",
+            "xarray==2022.3.0",
             "minian @ git+https://github.com/kushalbakshi/minian.git",
         ]
     },
